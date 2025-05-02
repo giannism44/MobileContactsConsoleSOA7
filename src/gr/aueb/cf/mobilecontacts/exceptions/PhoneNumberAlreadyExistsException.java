c18330c5696ef009b74ec42de72884783ff3f556
Ioannis Michos <giannismichos004@gmail.com>
2025-05-02 14:33:24 +0300

    Προσθήκη Main και Serializer, αλλαγές σε Controller, Mapper και Service

--- src/gr/aueb/cf/mobilecontacts/exceptions/PhoneNumberAlreadyExistsException.java
+++ src/gr/aueb/cf/mobilecontacts/exceptions/PhoneNumberAlreadyExistsException.java
@@ -1,6 +1,11 @@
 package gr.aueb.cf.mobilecontacts.exceptions;
 
+import java.io.Serial;
+
 public class PhoneNumberAlreadyExistsException extends  Exception{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public PhoneNumberAlreadyExistsException(String massage){
         super(massage);
--- src/gr/aueb/cf/mobilecontacts/service/MobileContactServiceImpl.java
+++ src/gr/aueb/cf/mobilecontacts/service/MobileContactServiceImpl.java
@@ -5,6 +5,7 @@ import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
 import gr.aueb.cf.mobilecontacts.dto.MobileContactUpdateDTO;
 import gr.aueb.cf.mobilecontacts.exceptions.ContactNotFoundException;
 import gr.aueb.cf.mobilecontacts.exceptions.PhoneNumberAlreadyExistsException;
+import gr.aueb.cf.mobilecontacts.mapper.Mapper;
 import gr.aueb.cf.mobilecontacts.model.MobileContact;
 
 import java.util.List;
@@ -25,7 +26,7 @@ public class MobileContactServiceImpl implements IMobileContactService{
                 if (dao.phoneNumberExists(dto.getPhoneNumber())){
                     throw new PhoneNumberAlreadyExistsException("Contact with phone number " + dto.getPhoneNumber() + "already exists");
                 }
-                mobileContact = mapInsertDTOToContact(dto);
+                mobileContact = Mapper.mapInsertDTOToContact(dto);
 
                 System.err.printf("MobileContactServiceImpl logger: %s was insert.\n", mobileContact);
                 return dao.insert(mobileContact);
@@ -56,7 +57,7 @@ public class MobileContactServiceImpl implements IMobileContactService{
                         + "already exists and can not be updated.");
             }
 
-            newContact = mapUpdateDTOToContact(dto);
+            newContact =Mapper.mapUpdateDTOToContact(dto);
             System.err.printf("MobileContactServiceImpl Logger: %s was updated with new info: %s\n", mobileContact , newContact);
             return dao.update(dto.getId(), newContact);
         }catch (ContactNotFoundException | PhoneNumberAlreadyExistsException e) {
@@ -134,11 +135,4 @@ public class MobileContactServiceImpl implements IMobileContactService{
         }
     }
 
-    private MobileContact mapInsertDTOToContact(MobileContactInsertDTO dto){
-        return new MobileContact(null, dto.getFirstname(), dto.getLastname(), dto.getPhoneNumber());
-    }
-
-    private MobileContact mapUpdateDTOToContact(MobileContactUpdateDTO dto){
-        return new MobileContact(dto.getId(), dto.getFirstname(), dto.getLastname(), dto.getPhoneNumber());
-    }
 }
